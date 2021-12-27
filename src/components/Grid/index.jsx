3b5ea9f539fda24362ec6a1c9645b7c988bb1582
Paul H Howells <p.h.howells@gmail.com>
2021-12-27 21:38:25 +0000

    feat: add rowGroupPanelShow toggle

--- src/components/Grid/index.jsx
+++ src/components/Grid/index.jsx
@@ -58,34 +58,89 @@ function Grid () {
 		onFilterChanged,
 	} = useGridState({persistedState, persistState});
 
+	const [gridKey, setGridKey] = React.useState(0);
+	// Don’t toggle between Tree Data and Grouping in an app. Pick one only.
+	// This is only done here for the demo.
+	const [showTreeData, setShowTreeData] = React.useState(false);
+
+	// When using Grouping it may be desirable to toggle the appearance of
+	// the Row Group Panel UI by using rowGroupPanelShow.  This example
+	// demonstrates how this may be done using a key.
+	const [showRowGroupPanel, setShowRowGroupPanel] = React.useState('always');
+
   return (
-    <div className="ag-theme-alpine grid">
-      <AgGridReact
-        columnDefs={columnDefs}
-        defaultColDef={defaultColDef}
-        rowData={rowData}
-				width="100%"
-				height="100%"
-        // onGridReady={onGridReady}
-				//
-        treeData
-				getDataPath={getDataPath}
-				groupDefaultExpanded={1}
-        autoGroupColumnDef={autoGroupColumnDef}
-				//
-				onFirstDataRendered={onFirstDataRendered}
-				onFilterChanged={onFilterChanged}
-				onSortChanged={onGridColumnStateChanged}
-				onColumnMoved={onGridColumnStateChanged}
-				onColumnPinned={onGridColumnStateChanged}
-				onColumnPivotChanged={onGridColumnStateChanged}
-				onColumnResized={onGridColumnStateChanged}
-				onColumnRowGroupChanged={onGridColumnStateChanged}
-				onColumnValueChanged ={onGridColumnStateChanged}
-				onColumnVisible={onGridColumnStateChanged}
-      />
-    </div>
+		<>
+			<button type="button" onClick={toggleShowTreeData}>
+				Tree Data / Grouping : { showTreeData ? 'Showing Tree Data' : 'Showing Grouping' }
+			</button>
+			{' '}
+			<button type="button" onClick={toggleShowRowGroupPanel}>
+				Group Panel : { (showRowGroupPanel ===  'always') ? 'on' : 'off' }
+			</button>
+
+			<div className="ag-theme-alpine grid">
+				<AgGridReact
+					columnDefs={columnDefs}
+					defaultColDef={defaultColDef}
+					rowData={rowData}
+					width="100%"
+					height="100%"
+					// onGridReady={onGridReady}
+					//
+					treeData={showTreeData}
+					getDataPath={getDataPath}
+					groupDefaultExpanded={1}
+					autoGroupColumnDef={autoGroupColumnDef}
+					//
+					onFirstDataRendered={onFirstDataRendered}
+					onFilterChanged={onFilterChanged}
+					onSortChanged={onGridColumnStateChanged}
+					onColumnMoved={onGridColumnStateChanged}
+					onColumnPinned={onGridColumnStateChanged}
+					onColumnPivotChanged={onGridColumnStateChanged}
+					onColumnResized={onGridColumnStateChanged}
+					onColumnRowGroupChanged={onGridColumnStateChanged}
+					onColumnValueChanged ={onGridColumnStateChanged}
+					onColumnVisible={onGridColumnStateChanged}
+					//
+					suppressColumnMoveAnimation
+					suppressDragLeaveHidesColumns
+					rowGroupPanelShow={showRowGroupPanel}
+					key={gridKey}
+				/>
+			</div>
+		</>
   );
+
+	function toggleShowTreeData () {
+		const newShowTreeDataState = !showTreeData;
+
+		setShowTreeData(newShowTreeDataState);
+
+		// If using Grouping instead of Tree Data then show Row Group Panel, and
+		// if using Tree Data instead of Gropuing then hide Row Group Panel
+		// setShowRowGroupPanel((newShowTreeDataState) ? '' : 'always');
+
+		// Hide RowGroupPanel if it’s redundant.
+		if (newShowTreeDataState) {
+			setShowRowGroupPanel('');
+		}
+
+		// AgGridReact does not respond when the treeData prop is updated,
+		// so hack to reset react component.
+		setGridKey((gridKey === 0) ? 1 : 0);
+	}
+
+	function toggleShowRowGroupPanel () {
+		const newState = (showRowGroupPanel ===  'always') ? '' : 'always';
+
+		setShowRowGroupPanel(newState);
+
+		// AgGridReact does not respond when the rowGroupPanelShow prop is updated,
+		// so hack to reset react component.
+		setGridKey((gridKey === 0) ? 1 : 0);
+	}
+
 }
 
 export default Grid;
